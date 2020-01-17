626338f84ebd72e40715a9b763c07d3da3a04c6b
renso3x <romeo.enso93@gmail.com>
2020-01-17 16:44:42 +0800

    config server find public path

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-const publicPath = path.join(__dirname, '..', 'dist');
+const publicPath = path.join(__dirname, 'dist');
 
 app.use(express.static(publicPath));
 
